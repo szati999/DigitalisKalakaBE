64f560f6196c77f4d8433e29c7eb74cbde2735c9
Robi-CamelCoding <robi@camelcoding.com>
2021-12-10 19:24:08 +0200

    firebase join

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express from 'express'
-
+import * as admin from 'firebase-admin';
+import { initializeApp } from 'firebase-admin';
+import { applicationDefault } from 'firebase-admin/app';
+const serviceAccount = require("../serviceAccountKeys.json");
 const app = express()
 const PORT = 3000
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://digitalis-kalaka.firebaseio.com'
+});
 app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
+    admin.firestore().collection("mivan").add({message: "faszagyerek"});
 })
+
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`)
 })
